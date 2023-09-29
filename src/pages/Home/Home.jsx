a1f8bd94c494bbc92f1dcc42547774125d299a67
Kopos <carlosh.viveros12@gmail.com>
2023-09-29 13:55:21 +0200

    Series_Router

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <main className="home">
+      <img src="https://valenciaplaza.com/public/Image/2016/12/culturplaza_66_02_forCrop.jpg" />
       <h1>Welcome to Series Router</h1>
     </main>
   );
